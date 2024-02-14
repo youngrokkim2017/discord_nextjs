fb6d135c5c9c71f1fd6127bd13b2116b26b186d4
Patrick Kim <patrick.yrkim@gmail.com>
2024-02-14 01:24:08 -0800

    Add fileupload component

--- components/modals/Initial-modal.tsx
+++ components/modals/Initial-modal.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as z from "zod";
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import {
@@ -21,7 +22,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+
+import { FileUpload } from "@/components/ui/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -70,7 +72,21 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField 
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload 
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField 
                                 control={form.control}
---
+++ components/ui/file-upload.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+interface FileUploadProps {
+    onChange: (url?: string) => void
+    value: string
+    endpoint: "messageFile" | "serverImage"
+}
+
+export const FileUpload = ({
+    onChange,
+    value, 
+    endpoint,
+}: FileUploadProps) => {
+    return (
+        <div>
+            File Upload Component
+        </div>
+    )
+}
