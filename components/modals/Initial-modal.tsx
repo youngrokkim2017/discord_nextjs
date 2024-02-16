5eb4efd9d80e370dd2300e71990b046d1ce6ade4
Patrick Kim <patrick.yrkim@gmail.com>
2024-02-15 23:00:33 -0800

    Add axios

--- components/modals/Initial-modal.tsx
+++ components/modals/Initial-modal.tsx
@@ -2,6 +2,7 @@
 
 import * as z from "zod";
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import {
@@ -22,6 +23,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 import { FileUpload } from "@/components/ui/file-upload";
 
@@ -36,6 +38,7 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
+    const router = useRouter()
 
     useEffect(() => {
         setIsMounted(true);
@@ -52,7 +55,15 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post("/api/servers", values)
+
+            form.reset()
+            router.refresh()
+            window.location.reload()
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     if (!isMounted) return null;
