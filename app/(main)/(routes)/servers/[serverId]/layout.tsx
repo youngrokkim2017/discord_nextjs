34d413ae1aa65d0bf8b4963e54bc0a47c27580c9
Patrick Kim <patrick.yrkim@gmail.com>
2024-02-18 15:25:42 -0800

    Add server sidebar

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,8 +1,9 @@
 import { redirectToSignIn } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
-import { redirect } from "next/navigation"
+import { ServerSidebar } from "@/components/server/server-sidebar"
 
 const ServerIdLayout = async ({
     children,
@@ -33,7 +34,9 @@ const ServerIdLayout = async ({
     return (
         <div className="h-full">
             <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-                server sidebare
+                <ServerSidebar 
+                    serverId={params.serverId}
+                />
             </div>
             <main className="h-full md:pl-60">
                 {children}
