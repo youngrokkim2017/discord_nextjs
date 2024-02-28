a9ba98be1a1ac3cca722919b89b20e6f92ed6bff
Patrick Kim <patrick.yrkim@gmail.com>
2024-02-27 22:47:12 -0800

    Update edit server modal

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -23,6 +23,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 import { FileUpload } from "@/components/ui/file-upload";
 import { useModal } from "@/hooks/use-modal-store";
@@ -37,10 +38,11 @@ const formSchema = z.object({
   });
 
 export const EditServerModal = () => {
-    const { isOpen, onClose, type } = useModal()
+    const { isOpen, onClose, type, data } = useModal()
     const router = useRouter()
 
     const isModalOpen = isOpen && type === "editServer"
+    const { server } = data
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -50,11 +52,18 @@ export const EditServerModal = () => {
         }
     })
 
+    useEffect(() => {
+        if (server) {
+            form.setValue("name", server.name)
+            form.setValue("imageUrl", server.imageUrl)
+        }
+    }, [server, form])
+
     const isLoading = form.formState.isSubmitting
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            await axios.post("/api/servers", values)
+            await axios.patch(`/api/servers/${server?.id}`, values)
 
             form.reset()
             router.refresh()
@@ -126,7 +135,7 @@ export const EditServerModal = () => {
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
                             <Button disabled={isLoading} variant="primary">
-                                Create
+                                Save
                             </Button>
                         </DialogFooter>
                     </form>
